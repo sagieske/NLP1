ff623d7174400b07d3d389b8e7b4173abc57ec88
David van Erkelens <dve@live.nl>
2014-11-16 20:01:04 +0100

    Crawler executed, now in posession of 12000+ lyrics

--- code/crawler.py
+++ code/crawler.py
@@ -30,27 +30,35 @@ def get_lyrics_document(url):
 	#Get artist name from url
 	song_name = url.split('/')[6].split('.')[0]
 	artist_name = url.split('/')[5]
-	print "Current artist: ", artist_name
+	
 	if(not artists_information.has_key(artist_name)):
 		#Get url for artist information from allmusic
-		artist_info_url = BeautifulSoup(urllib.urlopen('http://allmusic.com/search/artists/' + artist_name).read()).findAll('div', attrs={'class':'name'})[0].findNext('a')['href']
+		print "Current artist: ", artist_name
+		artist_info_url_tmp = BeautifulSoup(urllib.urlopen('http://allmusic.com/search/artists/' + artist_name).read()).findAll('div', attrs={'class':'name'})
+		if(len(artist_info_url_tmp) is 0):
+			#print "No data found for this artist, skipping..."
+			artists_information[artist_name] = "NOTFOUND"
+			return
+		artist_info_url = artist_info_url_tmp[0].findNext('a')['href']
 		#Get the Soup for the artist info
 		artist_info = BeautifulSoup(urllib.urlopen(artist_info_url).read())
 		artists_information[artist_name] = artist_info
 	
-	print "Retrieved artist data.."
+	#print "Retrieved artist data.."
 	artist_info = artists_information[artist_name]
 	
+	if(artist_info is "NOTFOUND"):
+		return
 	
 	#Retrieve genre info
 	if(artist_info is not None):
 		#Open writable file
-		f = open('lyrics_test/'+artist_name+"_"+song_name+'.txt', 'w')
+		f = open('lyrics/'+artist_name+"_"+song_name+'.txt', 'w')
 		genre_info_tmp = artist_info.find('div', attrs={'class':'genre'})
 		genre_info = 'unknown'
 		if(genre_info_tmp is not None):
 			genre_info = genre_info_tmp.find('a', href=True).get_text()
-		print "Retrieved genre: ", genre_info
+		#print "Retrieved genre: ", genre_info
 		#Retrieve subgenre info
 		subgenre_info = artist_info.findAll('div', attrs={'class':'styles'})
 		styles_list = []
@@ -59,13 +67,13 @@ def get_lyrics_document(url):
 			styles = div.findAll('a', href=True)
 			for style in styles:
 				styles_list.append(style.get_text())
-		print "Retrieved subgenres: ", str(styles_list)
-		print "Writing artist info to file '", artist_name + '_' + song_name + '.txt...'
+		#print "Retrieved subgenres: ", str(styles_list)
+		#print "Writing artist info to file '", artist_name + '_' + song_name + '.txt...'
 		f.write('Artist: ' + artist_name)
 		f.write('\nTitle: ' + song_name)
 		f.write('\nGenre: ' + genre_info)
 		f.write('\nSubgenres: ' + str(styles_list))
-		print "Getting cleaned lyrics.."
+		#print "Getting cleaned lyrics.."
 		text = BeautifulSoup(urllib.urlopen(url).read()).find('p', attrs={'id':"lyrics_text"})
 		print "Writing lyrics to file ", artist_name + '_' + song_name + '.txt...'
 		if(text is not None):
@@ -99,6 +107,7 @@ def get_artists(n=99):
 				if(a is not None and a['href'] is not '/'):
 					artist_url_list.append(a['href'])
 					artist_counter += 1
+
 	artist_counter = 0
 	soup = BeautifulSoup(urllib.urlopen('http://www.lyricsmode.com/lyrics/0-9').read())
 	if(soup is not None):
@@ -115,8 +124,6 @@ def get_artists(n=99):
 
 
 if __name__ == "__main__":
-	# get_lyrics_document('http://www.lyricsmode.com/lyrics/a/apo_hiking_society/panalangin.html')
-	# return 0
 
 	parser = argparse.ArgumentParser(description="Run crawler")
 	parser.add_argument('-n', metavar='How many artists should be retrieved per letter?', type=int)
@@ -137,9 +144,15 @@ if __name__ == "__main__":
 			time.sleep(1)
 		url_list += (get_top_songs(artist))
 	#Create lyrics documents
+	last_letter = 'a'
 	for url in url_list:
 		requests += 1
 		if(requests % 10 == 0):
 			print "Sleeping to prevent too many requests in too little time..."
 			time.sleep(1)
+		if(last_letter is not url.split('/')[5][0]):
+			last_letter = url.split('/')[5][0]
+			print "Sleeping before advancing to the next letter..."
+			time.sleep(15)
 		get_lyrics_document(url)
+	print "Retrieved ", requests, " lyrics documents.\n"
