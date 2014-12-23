de7a051c319d600f93e3f6344299dd7f7f0411a9
Elise <koster.elise@gmail.com>
2014-12-23 13:23:23 +0100

    word counts for test set

--- code/word_count_classifier.py
+++ code/word_count_classifier.py
@@ -17,7 +17,7 @@ beta = 0.1
 nr_runs = 1
 top_words = 50
 top_topics = 5
-lda = lda(alpha, beta, nr_topics, load_init=True)
+lda = lda(alpha, beta, nr_topics)
 
 # Do all 5 folds
 for i in range(0, 5):
@@ -34,7 +34,14 @@ for i in range(0, 5):
 	training_labels = lda.labels_dataset
 	test_set = lda.testset
 	test_set_labels = lda.labels_testset
-
+	for i in range(0, len(self.testset)):
+		cleaned_lyrics = self.testset[i]['cleaned_lyrics']
+		for j in range(0, len(cleaned_lyrics)): 
+			word = cleaned_lyrics[j]
+			# Update word count in total vocabulary
+			wordindex = self.vocab[word]
+			self.doc_word_testset[i][wordindex] += 1
+	print self.doct_word[i]
 	#print "Building test and training set..."
 	#number_documents = len(dataset)
 	#genre_list = []
@@ -51,13 +58,20 @@ for i in range(0, 5):
 	#classifier = svm.SVC()
 	#classifier.fit(training_set, training_genre)
 	classifier = svm.SVC(probability=True)
-	classifier.fit(training_set, training_genre)
+	classifier.fit(training_set, training_labels)
 
 
 	print "Testing classifier..."
 	predicted_genres = classifier.predict_proba(test_set)
 	print predicted_genres
 
+	list_genres = list(set(training_labels))
+	for genre_label in list_genres:
+		metrics = lda.calculate_metrics( test_set_labels, predicted_genres, genre_label)
+		print genre_label
+		print "metrics: ", metrics
+
+'''
 	test_number = len(test_genre)
 	correct = 0
 	incorrect = 0
@@ -68,7 +82,7 @@ for i in range(0, 5):
 			incorrect += 1
 	print "Correctly classified: ", correct
 	print "Incorrectly classified: ", incorrect
-
+'''
 	#Get the wordindex of word love:
 	#wordindex = lda.vocab['love']
 
