8b2904d4477dc1bac29632e542a276f9360731b0
sagieske <sharongieske@gmail.com>
2014-12-23 12:49:07 +0100

    added training test set

--- code/word_count_classifier.py
+++ code/word_count_classifier.py
@@ -19,51 +19,62 @@ top_words = 50
 top_topics = 5
 lda = lda(alpha, beta, nr_topics, load_init=True)
 
-#Get info about documents
-dataset = lda.dataset
-#Get word counts per document
-word_counts = lda.doc_word
+# Do all 5 folds
+for i in range(0, 5):
+	# Set fold number
+	lda.reset_to_next_fold(i)
+	#Get info about documents
+	dataset = lda.dataset
+	#Get word counts per document
+	word_counts = lda.doc_word
 
-print "Building test and training set..."
-number_documents = len(dataset)
-genre_list = []
-for doc_index in range(0, number_documents):
-	genre = dataset[doc_index]['genre']
-	genre_list.append(genre)
+	# THESE ARE THE SETS NEEDED FOR THIS FOLD
+	print "Building test and training set..."
+	training_set = lda.dataset
+	training_labels = lda.labels_dataset
+	test_set = lda.testset
+	test_set_labels = lda.labels_testset
 
-training_set = word_counts[:6000]	
-test_set = word_counts[6000:]
-training_genre = genre_list[:6000]
-test_genre = genre_list[6000:]
+	#print "Building test and training set..."
+	#number_documents = len(dataset)
+	#genre_list = []
+	#for doc_index in range(0, number_documents):
+	#	genre = dataset[doc_index]['genre']
+	#	genre_list.append(genre)
 
-print "Training classifier..."
-#classifier = svm.SVC()
-#classifier.fit(training_set, training_genre)
-classifier = svm.SVC(probability=True)
-classifier.fit(training_set, training_genre)
+	#training_set = word_counts[:6000]	
+	#test_set = word_counts[6000:]
+	#training_genre = genre_list[:6000]
+	#test_genre = genre_list[6000:]
+
+	print "Training classifier..."
+	#classifier = svm.SVC()
+	#classifier.fit(training_set, training_genre)
+	classifier = svm.SVC(probability=True)
+	classifier.fit(training_set, training_genre)
 
 
-print "Testing classifier..."
-predicted_genres = classifier.predict_proba(test_set)
-print predicted_genres
+	print "Testing classifier..."
+	predicted_genres = classifier.predict_proba(test_set)
+	print predicted_genres
 
-test_number = len(test_genre)
-correct = 0
-incorrect = 0
-for comparison in range(0, test_number):
-	if predicted_genres[comparison] == test_genre[comparison]:
-		correct +=1
-	else:
-		incorrect += 1
-print "Correctly classified: ", correct
-print "Incorrectly classified: ", incorrect
+	test_number = len(test_genre)
+	correct = 0
+	incorrect = 0
+	for comparison in range(0, test_number):
+		if predicted_genres[comparison] == test_genre[comparison]:
+			correct +=1
+		else:
+			incorrect += 1
+	print "Correctly classified: ", correct
+	print "Incorrectly classified: ", incorrect
 
-#Get the wordindex of word love:
-#wordindex = lda.vocab['love']
+	#Get the wordindex of word love:
+	#wordindex = lda.vocab['love']
 
-#Get the counts of the word love in document 0:
-#docindex = 0
-#lda.doc_word[docindex,wordindex]
+	#Get the counts of the word love in document 0:
+	#docindex = 0
+	#lda.doc_word[docindex,wordindex]
 
-#Get word corresponding to vocabulary index:
-#lda.index_to_vocab[wordindex]
+	#Get word corresponding to vocabulary index:
+	#lda.index_to_vocab[wordindex]
